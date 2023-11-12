d0751d3ec0c5f083c8c0ce8765d435aa36828292
wahyu <csetiawan@hijra.id>
2023-11-12 13:45:50 +0700

    Initial Commit

---
+++ chattingApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  chattingApp
+//
+//  Created by Wahyu Setiawan on 12/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
